e7c650df16da277fa7b338788d17856d672a3946
Remy Martin <moneyp10000@gmail.com>
2020-03-18 10:25:00 +0700

    2

--- ESP8266/esp8266_send_to_blynk_error/esp8266_send_to_blynk_error.ino
+++ ESP8266/esp8266_send_to_blynk_error/esp8266_send_to_blynk_error.ino
@@ -1,57 +1,37 @@
-#define BLYNK_PRINT Serial
+/*
+  Blink
 
+  Turns an LED on for one second, then off for one second, repeatedly.
 
-#include <ESP8266WiFi.h>
-#include <BlynkSimpleEsp8266.h>
-#include <SimpleDHT.h>
-#include <SimpleTimer.h>
+  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
+  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
+  the correct LED pin independent of which board is used.
+  If you want to know what pin the on-board LED is connected to on your Arduino
+  model, check the Technical Specs of your board at:
+  https://www.arduino.cc/en/Main/Products
 
-// DHT Config
-int pinDHT22 = D5;
-SimpleDHT22 dht22;
+  modified 8 May 2014
+  by Scott Fitzgerald
+  modified 2 Sep 2016
+  by Arturo Guadalupi
+  modified 8 Sep 2016
+  by Colby Newman
 
-// SET Timer
-SimpleTimer timer;
+  This example code is in the public domain.
 
-// You should get Auth Token in the Blynk App.
-// Go to the Project Settings (nut icon).
-char auth[] = " qchomkKyG9aMHI8n4iziH9P-BrniHoSk";
+  http://www.arduino.cc/en/Tutorial/Blink
+*/
 
-// Your WiFi credentials.
-// Set password to "" for open networks.
-char ssid[] = "Eleceasy";
-char pass[] = "Bb20072536";
-
-BLYNK_CONNECTED() {
-    Blynk.syncAll();
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void setup()
-{
-  // Debug console
-  Serial.begin(9600);
-
- 
-  // You can also specify server:
-  Blynk.begin(auth, ssid, pass, "oasiskit.com", 8080);
-  timer.setInterval(4000L, sendTemp);
-}
-
-void sendTemp()
-{
-  float temperature = 0;
-  float humidity = 0;
-  int err = SimpleDHTErrSuccess;
-  //if ((err = dht22.read2(pinDHT22, &temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
-    //Serial.print("Read DHT22 failed, err="); Serial.println(err);delay(1000);
-    //return;
- // }  
-  Blynk.virtualWrite(10, temperature); // virtual pin 
-  Blynk.virtualWrite(11, humidity); // virtual pin 
-}
-
-void loop()
-{
-  Blynk.run();
-  timer.run();
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
 }
