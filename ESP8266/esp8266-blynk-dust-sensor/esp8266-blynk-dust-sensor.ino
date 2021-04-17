45c1e150ffd4b34e9fa21c4c756f137c152c6426
Remy Martin <moneyp10000@gmail.com>
2021-04-17 13:15:51 +0700

    update v5 error

--- ESP8266/esp8266-blynk-dust-sensor/esp8266-blynk-dust-sensor.ino
+++ ESP8266/esp8266-blynk-dust-sensor/esp8266-blynk-dust-sensor.ino
@@ -2,7 +2,7 @@
 #include <TridentTD_LineNotify.h>
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
-#define DHTPIN D4 // what digital pin we're connected to
+#define DHTPIN D4     // what digital pin we're connected to
 #define DHTTYPE DHT11 // DHT 11
 //#define DHTTYPE DHT22 // DHT 22 (AM2302), AM2321
 //#define DHTTYPE DHT21 // DHT 21 (AM2301)
@@ -22,38 +22,39 @@ float dustDensity = 0;
 float d = dustDensity;
 float t; // Declare the variables
 float h;
+bool lock = false;
 WidgetLCD lcd(V0);
 void setup()
 {
-Serial.begin(9600);
-pinMode(ledPower, OUTPUT);
-Serial.println("DHTxx test!");
-Blynk.begin(auth, "WTF", "xxxxxxxxx0!", "blynk.honey.co.th", 8080);
-Serial.println(LINE.getVersion());
-// Serial.printf("WiFi connecting to %s\n");
-// while (WiFi.status() != WL_CONNECTED) {
-// Serial.print(".");
-// delay(400);
-// }
-Serial.println(WiFi.localIP());
-LINE.setToken(LINE_TOKEN);
-LINE.notify("PM 2.5: ");
-dht.begin();
-lcd.clear(); //Use it to clear the LCD Widget
-lcd.print(1, 0, "Air measurement"); // use: (position X: 0-15, position Y: 0-1, "Message you want to print")
-lcd.print(5, 1, "Monitor");
+    Serial.begin(9600);
+    pinMode(ledPower, OUTPUT);
+    Serial.println("DHTxx test!");
+    Blynk.begin(auth, "WTF", "xxxxxxxxx0!", "blynk.honey.co.th", 8080);
+    Serial.println(LINE.getVersion());
+    // Serial.printf("WiFi connecting to %s\n");
+    // while (WiFi.status() != WL_CONNECTED) {
+    // Serial.print(".");
+    // delay(400);
+    // }
+    Serial.println(WiFi.localIP());
+    LINE.setToken(LINE_TOKEN);
+    LINE.notify("PM 2.5: ");
+    dht.begin();
+    lcd.clear();                        //Use it to clear the LCD Widget
+    lcd.print(1, 0, "Air measurement"); // use: (position X: 0-15, position Y: 0-1, "Message you want to print")
+    lcd.print(5, 1, "Monitor");
 }
-BLYNK_WRITE(V5){
-Serial.println(param.asInt() );
-if (param.asInt()== 1){
-LINE.notify("PM 2.5: "+String(dustDensity)+" µg/m³");
-} else {
-lcd.clear();
-lcd.print(3,0,"Off The Light");
-lcd.print(5,1,"<<NOW>>");
-// LED.off();
-// digitalWrite(LED1, LOW);
-}
+BLYNK_WRITE(V5)
+{
+
+     if (param.asInt()==1) {
+         lock = true;
+
+ }else{
+         lock = false;
+
+ }
+
 }
 //BLYNK_WRITE(V5)
 //{
@@ -67,45 +68,56 @@ lcd.print(5,1,"<<NOW>>");
 //}
 void loop()
 {
-Blynk.run();
-timer.run();
-digitalWrite(ledPower, LOW); // power on the LED
-delayMicroseconds(samplingTime);
-voMeasured = analogRead(measurePin); // read the dust value
-delayMicroseconds(deltaTime);
-digitalWrite(ledPower, HIGH); // turn the LED off
-delayMicroseconds(sleepTime);
-// 0 - 3.3V mapped to 0 - 1023 integer values
-// recover voltage
-calcVoltage = voMeasured * (3.3 / 1024);
-dustDensity = 0.17 * calcVoltage - 0.1;
-dustDensity = dustDensity * 1000;
-Serial.print(" - Dust Density: ");
-Serial.print(dustDensity);
-Serial.println(" µg/m³");
-delay(1000);
-Blynk.virtualWrite(V3, dustDensity);
-Blynk.virtualWrite(V4, voMeasured);
-float h = dht.readHumidity();
-float t = dht.readTemperature();
-float f = dht.readTemperature(true);
-// เช็คถ้าอ่านค่าไม่สำเร็จให้เริ่มอ่านใหม่
-if (isnan(h) || isnan(t) || isnan(f))
-{
-Serial.println("Failed to read from DHT sensor!");
-return;
+    Blynk.run();
+    timer.run();
+    digitalWrite(ledPower, LOW); // power on the LED
+    delayMicroseconds(samplingTime);
+    voMeasured = analogRead(measurePin); // read the dust value
+    delayMicroseconds(deltaTime);
+    digitalWrite(ledPower, HIGH); // turn the LED off
+    delayMicroseconds(sleepTime);
+    // 0 - 3.3V mapped to 0 - 1023 integer values
+    // recover voltage
+    calcVoltage = voMeasured * (3.3 / 1024);
+    dustDensity = 0.17 * calcVoltage - 0.1;
+    dustDensity = dustDensity * 1000;
+    Serial.print(" - Dust Density: ");
+    Serial.print(dustDensity);
+    Serial.println(" µg/m³");
+    delay(1000);
+    Blynk.virtualWrite(V3, dustDensity);
+    Blynk.virtualWrite(V4, voMeasured);
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+    float f = dht.readTemperature(true);
+    // เช็คถ้าอ่านค่าไม่สำเร็จให้เริ่มอ่านใหม่
+    if (isnan(h) || isnan(t) || isnan(f))
+    {
+        Serial.println("Failed to read from DHT sensor!");
+        return;
+    }
+    Serial.print("Humidity: ");
+    Serial.print(h);
+    Serial.print(" %\t");
+    Serial.print("Temperature: ");
+    Serial.print(t);
+    Serial.print(" *C ");
+    Serial.print(f);
+    Serial.print(" *F\t\n");
+    Blynk.virtualWrite(V1, t);
+    Blynk.virtualWrite(V2, h);
+    // if (pm1>=30){
+    // LINE.notify("PM 1.0: "+String(pm1)+" ug/m3");
+    // }
+     if (lock == true)
+    {
+        LINE.notify("PM 2.5: " + String(dustDensity) + " µg/m³");
+        Blynk.virtualWrite(V5, LOW);
+        lock = false;
+    }else
+    {
+        lcd.clear();
+        lcd.print(3, 0, "Off The Light");
+        lcd.print(5, 1, "<<NOW>>");
+    }
 }
-Serial.print("Humidity: ");
-Serial.print(h);
-Serial.print(" %\t");
-Serial.print("Temperature: ");
-Serial.print(t);
-Serial.print(" *C ");
-Serial.print(f);
-Serial.print(" *F\t\n");
-Blynk.virtualWrite(V1, t);
-Blynk.virtualWrite(V2, h);
-// if (pm1>=30){
-// LINE.notify("PM 1.0: "+String(pm1)+" ug/m3");
-// }
-} 
