48303be9fc9a2c70d12499323d70d571767742e9
Permpolthan <moneyp1000@gmail.com>
2021-10-27 21:50:33 +0700

    หาค่าเฉลี่ย ความชื้น

--- ESP8266/avr_soil/avr_soil.ino
+++ ESP8266/avr_soil/avr_soil.ino
@@ -1,13 +1,22 @@
+int _sensorValue[3];
+int avr_sensorValue;
+
 void sendSoilValue() {
-  sensorValue = analogRead(A0);
-  percentageHumididy = map(sensorValue, 0, 1024, 0, 100);
 
-  Blynk.virtualWrite(V5, percentageHumididy);
-  Blynk.virtualWrite(V0, sensorValue);
+  for (int i = 0; i >= 3; i++) {
+    _sensorValue[i] = analogRead(A0);
+  }
 
-  Serial.print(percentageHumididy);
-  Serial.println("%");
-  Serial.print("A0 = ");
-  Serial.println(sensorValue);
-  delay(100);
+  if (i == 3) {
+    avr_sensorValue = ( _sensorValue[0]+ _sensorValue[1]+ _sensorValue[2])/3;
+    percentageHumididy = map(avr_sensorValue, 0, 1024, 0, 100);
+    Blynk.virtualWrite(V5, percentageHumididy);
+    Blynk.virtualWrite(V0, avr_sensorValue);
+    Serial.print(percentageHumididy);
+    Serial.println("%");
+    Serial.print("A0 = ");
+    Serial.println(avr_sensorValue);
+    delay(100);
+  }
+
 }
